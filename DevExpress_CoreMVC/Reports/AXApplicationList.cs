52063464e624e3b4dd3831a7b9ba7bf7711db15e
Jennifer M Reusser <jennifer.reusser@fnsb.gov>
2021-04-09 15:53:28 -0800

    Add project files.

---
+++ DevExpress_CoreMVC/Reports/AXApplicationList.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace DevExpress_CoreMVC.Reports
+{
+    public partial class AXApplicationList
+    {
+        public AXApplicationList()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DevExpress_CoreMVC/Reports/AXGlobalUDLList.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace DevExpress_CoreMVC.Reports
+{
+    public partial class AXGlobalUDLList
+    {
+        public AXGlobalUDLList()
+        {
+            InitializeComponent();
+        }
+    }
+}
